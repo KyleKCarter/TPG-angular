4011bcf9a5c92463ff014aa567a3bc7e48ce579a
kyle <kkcarter1129@gmail.com>
2024-10-02 16:52:02 -0400

    fixing issue with ride response

--- src/app/attraction/attraction.component.ts
+++ src/app/attraction/attraction.component.ts
@@ -19,8 +19,8 @@ export class AttractionComponent implements OnInit {
 
   attraction!: string;
   park!: string;
-  // rides: RideResponse;
-  rides: any;
+  rides?: RideResponse[];
+  // rides: any;
   ride: any;
 
   constructor(
@@ -48,7 +48,7 @@ export class AttractionComponent implements OnInit {
           if (this.rides[i].name === this.attraction) {
             this.ride = this.rides[i]
           }
-          console.log(this.rides)
+          // console.log(this.rides)
         }
       }
     })
